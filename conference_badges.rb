821f432a65e17d5ffc767b796159b5af297b7eae
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-10 04:00:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,8 @@ def batch_badge_creator names
   names.map {|name| badge_maker name}
 end
 
-def assign_rooms name
-
+def assign_rooms names
+  names.map.with_index(1) {|name, room| "Hello, #{name}! You'll be assigned to room #{room}!"}
 end
 
 def printer name
